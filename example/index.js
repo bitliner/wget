e6e58aa6b4d5a73a817dc2b486c5ae19e618ae16
bitliner <giovanni.gaglione@gmail.com>
2013-10-20 01:55:50 +0200

    different example

--- example/index.js
+++ example/index.js
@@ -1,2 +1,2 @@
 var wget=require('../index.js')
-wget.run('http://stackoverflow.com/questions/8986139/wget-output-directory-prefix')
+wget.run('http://stackoverflow.com/questions/8986139/wget-output-directory-prefix','../')
